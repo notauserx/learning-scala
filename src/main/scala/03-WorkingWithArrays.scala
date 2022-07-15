97ea4126c56ae7c65c7053f638568c7304be02cb
notauserx <notauserx@users.noreply.github.com>
2022-07-15 16:17:25 +0600

    fix formatting

--- src/main/scala/03-WorkingWithArrays.scala
+++ src/main/scala/03-WorkingWithArrays.scala
@@ -1,9 +1,10 @@
 package WorkingWithArrays
 
 class WorkingWithArrays {
-
-    // Write a loop that swaps adjacent elements of an array of integers. For example,
-    // Array(1, 2, 3, 4, 5) becomes Array(2, 1, 4, 3, 5).
+    /* Ex 2
+    //  Write a loop that swaps adjacent elements of an array of integers. For example,
+    //  Array(1, 2, 3, 4, 5) becomes Array(2, 1, 4, 3, 5).
+    */
     def swapAdjacent(a : Array[Int]) = {
         for (i <- 0 until a.length if i % 2 == 1) {val t = a(i); a(i) = a(i-1); a(i-1) = t }
     }
