c6c68b7e055c924ee1854f836330c64f568b39c0
notauserx <notauserx@users.noreply.github.com>
2022-07-15 16:16:08 +0600

    add solution for ex 3.2

--- src/main/scala/03-WorkingWithArrays.scala
+++ src/main/scala/03-WorkingWithArrays.scala
@@ -1,2 +1,15 @@
 package WorkingWithArrays
 
+class WorkingWithArrays {
+
+    // Write a loop that swaps adjacent elements of an array of integers. For example,
+    // Array(1, 2, 3, 4, 5) becomes Array(2, 1, 4, 3, 5).
+    def swapAdjacent(a : Array[Int]) = {
+        for (i <- 0 until a.length if i % 2 == 1) {val t = a(i); a(i) = a(i-1); a(i-1) = t }
+    }
+
+    def swapAdjacentAndReturn(a : Array[Int]) : Array[Int] = {
+        for (i <- 0 until a.length if i % 2 == 1) {val t = a(i); a(i) = a(i-1); a(i-1) = t }
+        a
+    }
+}
--- src/test/scala/03-WorkingWithArraysTests.scala
+++ src/test/scala/03-WorkingWithArraysTests.scala
@@ -1,1 +1,24 @@
 package WorkingWithArrays
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest._
+import matchers.should._
+
+class WorkingWithArraysTests extends AnyFunSuite with Matchers {
+    val ins = new WorkingWithArrays()
+    // test exercise 2
+    test("array swap with single element") {
+        var prob = Array(1)
+        ins.swapAdjacent(prob) 
+        print(prob)
+        prob should equal (Array(1))
+
+    }
+
+    test("array swap with two elements") {
+        var prob = Array(1,2)
+        ins.swapAdjacent(prob) 
+        prob should equal (Array(2,1))
+
+    }
+}
