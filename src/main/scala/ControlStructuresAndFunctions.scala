2f19cdf0e6fd093430d53f0b90db12d6927ffd7e
notauserx <notauserx@users.noreply.github.com>
2022-07-15 10:13:36 +0600

    add ch2 ex 11 with tests

--- src/main/scala/ControlStructuresAndFunctions.scala
+++ src/main/scala/ControlStructuresAndFunctions.scala
@@ -1,5 +1,7 @@
 package ControlStructuresAndFunctions
 
+import java.time.LocalDate
+
 trait Output {
     def print(s: String) = Console.println(s)
 }
@@ -10,12 +12,55 @@ class ControlStructuresAndFunctions  extends Output{
         else multiply(args.tail: _*) * args.head
     }
 
-    // 5. Write a procedure countdown(n: Int) that prints the numbers from n to 0.
     def countdown(n: Int)  = {
         for(i <- n to (0, -1)) yield i
     }
 
+    // 5. Write a procedure countdown(n: Int) that prints the numbers from n to 0.
     def countdownandprint(n: Int)  = {
         for(i <- n to (0, -1)) print(i+ "")
+    }
+}
+
+object Helpers {
+    /*
+    11. Define a string interpolator date so that you can define a java.time.LocalDate as
+    date"$year-$month-$day". You need to define an “implicit” class with a date method,
+    like this:
+    implicit class DateInterpolator(val sc: StringContext) extends AnyVal {
+    def date(args: Any*): LocalDate = . . .
+    }
+    args(i) is the value of the ith expression. Convert each to a string and then to
+    an integer, and pass them to the LocalDate.of method. If you already know
+    some Scala, add error handling. Throw an exception if there aren’t three
+    arguments, or if they aren’t integers, or if they aren’t separated by dashes.
+    (You get the strings in between the expressions as sc.parts.)
+    */
+    implicit class DateInterpolator(val sc: StringContext) extends AnyVal {
+        def date(args: Any*) : LocalDate = {
+            try {
+                if(args.length != 3) throw new IllegalArgumentException
+                    ("There has to be three arguments")
+
+                val (year, month, day) = (
+                        args(0).toString.toInt, 
+                        args(1).toString.toInt, 
+                        args(2).toString.toInt)
+
+                for(sep <- sc.parts) 
+                    if(sep.length > 0 && !sep.equals("-")) 
+                        throw new IllegalArgumentException("Arguments aren't seperated by dashes.")
+
+                LocalDate.of(year, month, day)
+
+            }
+            catch {
+                case ex: NumberFormatException => 
+                    println("Not a valid date input")
+                    throw ex
+                case ex: IllegalArgumentException =>
+                    throw ex
+            }
+        }
     }
 }
--- src/test/scala/ControlStructuresAndFunctionsTests.scala
+++ src/test/scala/ControlStructuresAndFunctionsTests.scala
@@ -1,8 +1,14 @@
 package ControlStructuresAndFunctions
 
+import java.time.LocalDate
+
+
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest._
 import matchers.should._
+
+import ControlStructuresAndFunctions.Helpers._
+
  
 trait MockOutput extends Output {
     var messages: Seq[String] = Seq()
@@ -11,7 +17,7 @@ trait MockOutput extends Output {
 }
 
 class ControlStructuresAndFunctionsTests extends AnyFunSuite with Matchers{
-    val ins = new ControlStructuresAndFunctions with MockOutput
+    val ins = new ControlStructuresAndFunctions
 
     // test
     test("the result of 5 times 4 times 3 times 2 times 1 is 120") {
@@ -44,6 +50,27 @@ class ControlStructuresAndFunctionsTests extends AnyFunSuite with Matchers{
     }
 
 
+    // test exercise 11
 
+    test("date\"$year-$month-$day\" doesn't throw IllegalArgumentException") {
+        val year = 2022
+        val month = 2
+        val day = 22
+        val result = date"$year-$month-$day"
+
+        val expected = LocalDate.of(year, month, day)
+        assert(result == expected)
+        //noException should be thrownBy result
+    }
+
+    test("date throws IllegalArgumentException for more that 3 args") {
+        val one,two,three,four = (1,2,3,4)
+        an [IllegalArgumentException] should be thrownBy date"$one-$two-$three-$four"
+    }
+
+    test("date throws NumberFormatException for date(1,2,3)") {
+        val one,two,three = (1,2,3)
+        an [IllegalArgumentException] should be thrownBy date"$one-$two-$three"
+    }
 
 }
