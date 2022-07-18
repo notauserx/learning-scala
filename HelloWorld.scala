898a1bd31889149f2439716bbb7b907d8fb6e153
notauserx <notauserx@users.noreply.github.com>
2022-07-18 12:52:56 +0600

    cleanup

--- HelloWorld.scala
+++ HelloWorld.scala
@@ -1,7 +1,4 @@
 import MapsAndTuples._
 object HelloWorld extends App {
     println("Hello, world")
-    val ins = new MapsAndTuples();
-    val l = ins.countWords()
-    for((k,v) <- l) println(k + " occurs " + v + "times.")
 }
