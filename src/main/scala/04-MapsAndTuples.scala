0446435c92a4be70dedee678cf34934f00bbdac0
notauserx <notauserx@users.noreply.github.com>
2022-07-18 12:53:27 +0600

    add code and test for ch4ex2

--- src/main/scala/04-MapsAndTuples.scala
+++ src/main/scala/04-MapsAndTuples.scala
@@ -1,5 +1,7 @@
 package MapsAndTuples
 
+import com.typesafe.config._
+
 class MapsAndTuples {
     def countWords() = {
         val wordCount : scala.collection.mutable.Map[String, Int] = scala.collection.mutable.Map()
@@ -10,6 +12,29 @@ class MapsAndTuples {
             if(wordCount.contains(item))
                 wordCount(item) += 1
             else wordCount(item) = 1
+        }
+        wordCount
+    }
+
+    def readFile() = {
+        val conf = ConfigFactory.load()
+
+        val fileName = conf.getString("app.mapsandtuplesFilePath")
+        val words = """([A-Za-z])+""".r
+        io.Source.fromFile(fileName)
+            .getLines
+            .flatMap(words.findAllIn)
+            .toList     
+    }
+
+
+    def countWords(source : List[String]) = {
+
+        val wordCount : scala.collection.mutable.Map[String, Int] = scala.collection.mutable.Map()
+        for(item <- source) {
+            if(wordCount.contains(item))
+                wordCount(item) += 1
+            else wordCount(item) = 1 
         }
         wordCount
     }
---
+++ src/test/scala/04-MapsAndTuplesTests.scala
@@ -0,0 +1,21 @@
+package MapsAndTuples
+
+import scala.collection.mutable.ArrayBuffer
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest._
+import matchers.should._
+
+class MapsAndTuplesTests extends AnyFunSuite with Matchers{
+    
+    val ins = new MapsAndTuples()
+    
+
+    test("mutable map should capture word frequencies") {
+        val words = "a"::"simple"::"test"::"for"::"a"::"map"::Nil
+
+        val freq = ins.countWords(words);
+
+        freq should equal (Map("a" -> 2, "simple" -> 1, "test" -> 1, "for"-> 1, "map" -> 1))
+    }
+
+}
